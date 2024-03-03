cc1bc054714d9c18adb091590790b3ac3fd4c10a
weijia <2112025700@qq.com>
2024-03-03 22:22:38 +0800

    feat: 前端项目引入 Antd 和 TailwindCSS

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,9 @@
-import "./App.css";
 import { xd } from "@lowcode/share";
+import { Button } from "antd";
 
 function App() {
   return <>{xd}</>;
+  <Button type="primary">Button</Button>;
 }
 
 export default App;
